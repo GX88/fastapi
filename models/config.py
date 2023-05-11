86cab79e01f3663bb3cd58552f3c0fa6383d4296
Faith <gxlove_max@163.com>
2023-05-11 18:24:18 +0800

    ...

--- models/config.py
+++ models/config.py
@@ -3,8 +3,8 @@ from .Mixin import TimestampMixin, fields
 
 class Config(TimestampMixin):
     id = fields.IntField(pk=True, description="主键")
-    name = fields.CharField(max_length=50, description="配置名称")
-    value = fields.JSONField(description="配置值")
+    name = fields.CharField(max_length=50, null=False, description="配置名称")
+    value = fields.JSONField(null=False, description="配置值")
 
     class Meta:
         table = "restart_config"
