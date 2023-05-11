da5efbdb53ece083e769343dbeefb4ea05eacf7d
Faith <gxlove_max@163.com>
2023-05-11 18:45:21 +0800

    ...

--- sql/config.sql
+++ sql/config.sql
@@ -1,4 +1,6 @@
-select *, id from `restart_tag`;
-
--- 注释
-# 注释
+-- ----------------------------
+-- Records of restart_config
+-- ----------------------------
+INSERT INTO `restart_config` VALUES (1, 'website', '{\"RecordCode\": \"网站备案号\", \"WebsiteUrl\": \"前台地址\", \"WebsiteLogo\": \"网站logo\", \"WebsiteName\": \"网站名称\", \"WebsiteEmail\": \"博主邮箱\", \"WebsiteAuthor\": \"网站博主\", \"WebsiteAvatar\": \"博主头像\", \"WebsiteKeywords\": \"网站关键字\", \"VerificationCode\": false, \"WebsiteDescription\": \"网站描述\"}', '2023-05-11 18:36:28.755953', '2023-05-11 18:36:28.755953');
+INSERT INTO `restart_config` VALUES (2, 'email', '{\"cc\": [\"抄送人邮箱\"], \"Host\": \"smtp.163.com\", \"Port\": 25, \"Email\": \"邮箱账号\", \"Ecoding\": \"utf-8\", \"FromName\": \"发件人名称\", \"Password\": \"邮箱密码\", \"Encryption\": [\"STARTTLS\", \"SSL\", \"TLS\"]}', '2023-05-11 18:38:09.494823', '2023-05-11 18:38:09.494823');
+INSERT INTO `restart_config` VALUES (3, 'article', '{\"cc\": [\"抄送人邮箱\"], \"Host\": \"smtp.163.com\", \"Port\": 25, \"Email\": \"邮箱账号\", \"Ecoding\": \"utf-8\", \"FromName\": \"发件人名称\", \"Password\": \"邮箱密码\", \"Encryption\": [\"STARTTLS\", \"SSL\", \"TLS\"]}', '2023-05-11 18:39:28.825082', '2023-05-11 18:39:28.825082');
